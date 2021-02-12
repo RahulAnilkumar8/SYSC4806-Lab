4e26e8b07342a5776034d6cf76bbe9e6448c89fe
Rahul <rahulanilkumar8@gmail.com>
2021-02-11 20:58:47 -0500

    Added integration tests

--- src/test/java/com/book/AddressBook/AddressBookApplicationTests.java
+++ src/test/java/com/book/AddressBook/AddressBookApplicationTests.java
@@ -1,13 +1,74 @@
 package com.book.AddressBook;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class AddressBookApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private AddressBookController controller;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
+
+	@Test
+	public void addBook() throws Exception{
+		AddressBook book = new AddressBook();
+		book.setBookName("Book 1");
+		mockMvc.perform( MockMvcRequestBuilders
+				.post("/addressbook")
+				.content(asJsonString(book))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+	}
+
+	@Test
+	public void getBuddy() throws Exception{
+		mockMvc.perform(get("/addressbook")).andDo(print()).andExpect(status().isOk());
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
 
 }
---
+++ src/test/java/com/book/AddressBook/AddressBookNonMockTest.java
@@ -0,0 +1,60 @@
+package com.book.AddressBook;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import org.springframework.http.ResponseEntity;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AddressBookNonMockTest {
+
+        @LocalServerPort
+        private int port;
+
+        @Autowired
+        private AddressBookController controller;
+
+        @Autowired
+        private TestRestTemplate restTemplate;
+
+        @Test
+        void contextLoads() {
+            assertThat(controller).isNotNull();
+        }
+
+	@Test
+	public void addBook() throws Exception{
+        AddressBook book = new AddressBook();
+        book.setBookName("Book 1");
+        ResponseEntity<String> result = this.restTemplate.postForEntity("http://localhost:" + port + "/addressbook", book , String.class);
+        assertEquals(200, result.getStatusCodeValue());
+	}
+
+    @Test
+    public void getBuddy() throws Exception{
+        ResponseEntity<String> result = this.restTemplate.getForEntity("http://localhost:" + port + "/addressbook", String.class);
+        assertEquals(200, result.getStatusCodeValue());
+    }
+
+    //	@Test
+//	public void addBuddy() throws Exception{
+//		AddressBook book = new AddressBook();
+//		book.setBookName("Book 2");
+//		ResponseEntity<String> result = this.restTemplate.postForEntity("http://localhost:" + port + "/addressbook", book , String.class);
+//		assertEquals(200, result.getStatusCodeValue());
+//		BuddyInfo buddy = new BuddyInfo("Rob", "23 HELLO Way", "1234567789");
+//		result = this.restTemplate.postForEntity("http://localhost:" + port + "/addressbook/"+book.getId()+"/BuddyInfo", buddy, String.class);
+//		assertEquals(200, result.getStatusCodeValue());
+//	}
+
+}
